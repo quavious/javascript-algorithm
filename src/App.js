13e45a9cb1edf857d6d37faff3d4da7b938b877e
quavious <leenowon97@gmail.com>
2020-04-28 14:53:41 +0900

    줄임말

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 
 function algo() {
-  var students = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일']
-  students.forEach((s, idx) => {
-    console.log(`번호 : ${idx+1}, 이름 : ${s}`)
-  })
+  var result = ""
+  var str = "Hello World To Javascript".split(" ").forEach(s => result += s[0])
+  return result
+
 }
 
 function App() {

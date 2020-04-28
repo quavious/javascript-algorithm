fcfedd859eb2d3955c3c4fa32b1cf725a425bc80
quavious <leenowon97@gmail.com>
2020-04-28 14:13:34 +0900

    연속되는 수

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function arrMerge(left, right){
-  let result = [];
-
-  while(left.length && right.length){
-    if(left[0] <= right[0])
-      result.push(left.shift());
-    else
-      result.push(right.shift());  
-  }
-  while(left.length) result.push(left.shift());
-  while(right.length) result.push(right.shift());
-
-  return result;
-}
-
 function algo(arr) {
-  if(arr.length <= 1) return arr;
-
-  const mid = Math.floor(arr.length/2);
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
-
-  return arrMerge(algo(left), algo(right));
+  var flag = true
+  for(var i=0;i<arr.length-1;i++){
+    if(Math.abs(arr[i+1]-arr[i]) !== 1){
+      flag = false;
+      break;
+    }
+  }
+  if(flag) return "YES";
+  else return "NO";
 }
 
 function App() {
-  var arr = "9 4 6 7 3 1 0 5 2 15 8 10".split(" ").map(i => parseInt(i, 10))
+  var arr = "10 9 8 7 6".split(" ").map(i => parseInt(i, 10))
   console.log(algo(arr));
   return <h1>HELLO WORLD!</h1>
 }

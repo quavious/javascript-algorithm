4da99ea485c7bfc25f727a9a343ac90793f66c6a
quavious <leenowon97@gmail.com>
2020-04-28 14:08:59 +0900

    mergesort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './App.css';
 
+function arrMerge(left, right){
+  let result = [];
+
+  while(left.length && right.length){
+    if(left[0] <= right[0])
+      result.push(left.shift());
+    else
+      result.push(right.shift());  
+  }
+  while(left.length) result.push(left.shift());
+  while(right.length) result.push(right.shift());
+
+  return result;
+}
+
 function algo(arr) {
   if(arr.length <= 1) return arr;
-  else {
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
 
-    for(let i=1; i<arr.length;i++){
-      if(arr[i]<=pivot) left.push(arr[i]);
-      else right.push(arr[i]);
-    }
-    return algo(left).concat(pivot, algo(right));
-  }
+  const mid = Math.floor(arr.length/2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return arrMerge(algo(left), algo(right));
 }
 
 function App() {
-  var arr = "9 4 6 7 3 1 0 5 2 15 10".split(" ").map(i => parseInt(i, 10))
+  var arr = "9 4 6 7 3 1 0 5 2 15 8 10".split(" ").map(i => parseInt(i, 10))
   console.log(algo(arr));
   return <h1>HELLO WORLD!</h1>
 }

28e68340b67e8306ce686ef84ead30ce9896c536
quavious <leenowon97@gmail.com>
2020-04-28 14:24:29 +0900

    bracket string

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from 'react';
 import './App.css';
 
 function algo(arr) {
-  var flag = true
-  for(var i=0;i<arr.length-1;i++){
-    if(Math.abs(arr[i+1]-arr[i]) !== 1){
-      flag = false;
-      break;
+  var flag1=0, flag2=0, flag3 = 0;
+  for(var i=0;i<arr.length;i++){
+    if(arr[i]==="(") flag1++;
+    else if(arr[i] === ")") flag1--;
+    else if(arr[i]==="{") flag2++;
+    else if(arr[i] === "}") flag2--;
+    else if(arr[i]==="[") flag3++;
+    else if(arr[i] === "]") flag3--;
+
+    if(flag1<0 || flag2<0 || flag3<0) {
+      return "NO";
     }
   }
-  if(flag) return "YES";
-  else return "NO";
+  if(flag1 === 0 && flag2 === 0 && flag3 === 0) return "YES"
+  return "NO";
 }
 
 function App() {
-  var arr = "10 9 8 7 6".split(" ").map(i => parseInt(i, 10))
+  var arr = "(()){[".split("")
   console.log(algo(arr));
   return <h1>HELLO WORLD!</h1>
 }

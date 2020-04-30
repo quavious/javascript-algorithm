1088e5be5c1638e29251628ad5a0f4320d00ee34
quavious <leenowon97@gmail.com>
2020-05-01 01:05:45 +0900

    소수 찾기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
 
+function makeTable(num){
+  const table = []
+  for(var i=0;i<=num;i++) table.push(true)
+  for(let j=2;j*j<=num;j++){
+    if(table[j]){
+      for(let k=j*j;k<=num;k+=j){
+        table[k] = false;
+      }
+    }
+  }
+  table.splice(0,2,false, false)
+  return table
+  
+}
+
 function algo() {
-  var r = []
-  var tower = ["ABCDEF","BCAD","ADEFQRX","BEDFG","EFGHZ"]
-  tower.forEach(t => {
-    if(t === t.split("").sort().join(""))
-      r.push("YES")
-    else
-      r.push("NO")  
-  })
-  return r
+  var num = 300
+  var table = makeTable(num);
+  var result = []
+  for(let n=2;n<=num/2;n++){
+    if(table[n] && table[num-n]){
+      result.push([n,num-n]);
+    }
+  }
+  return result
 }
 
 function App() {
